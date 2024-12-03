1b7cf0445f2923d26cfe22bf894bd675bf85d53d
Di-djo <luiz.g.comarella@gmail.com>
2024-12-03 00:09:50 -0300

    Added the possibility for guessing the whole word

--- client.py
+++ client.py
@@ -24,6 +24,7 @@ class main_game:
 
         if letra in self.lista_letras_usadas:
             print('Essa letra ja foi utilizada!')
+            return -1
         # Resolver problema de letras repetidas
         elif letra in self.lista_letras:
             print('Letra encontrada! \"' + letra + '\"')
@@ -59,7 +60,7 @@ class main_game:
 
         elif self.vidas == 0:
             cls()
-            print('Que pena, voce perdeu :<\nA palavra era: ' + self.palavra)
+            print('Que pena, voces perderam :<\nA palavra era: ' + self.palavra)
             self.fim_do_jogo = True
 
         else:
@@ -68,17 +69,18 @@ class main_game:
         return self.fim_do_jogo
 
     def escreve_palavra(self):
-
+        output = ''
         for i in range(self.tamanho_original):
             # se letra ja encontrada, printa a letra e um espaco
             if self.lista_letras[i] in self.lista_letras_usadas:
-                print(self.lista_letras[i] + ' ')
+                output+=self.lista_letras[i] + ' '
             else:
-                print('_ ')
+                output+='_ '
+        print(output)
 
     def tenta_letra(self):
         letra = input('Digite uma letra que deseja testar:')
-        return letra
+        return letra.capitalize()
 
 
 def main():
@@ -102,27 +104,33 @@ def main():
 
     forca = main_game(palavra)
 
-    encerrado = False
+    encerrado = forca.fim_do_jogo
 
     while not encerrado:
-
+        cls()
         codigo = jogador.recv(1)
         if not codigo:
             sys.exit(-1)
+
         codigo = codigo.decode()
+
         if codigo == 'L':
             letra = jogador.recv(1)
             letra = letra.decode()
             forca.verifica_letra_oponente(letra)
+
             verificacao = forca.verifica_fim_do_jogo()  # Atualizar verificação para oponente
+            
             if verificacao == True and forca.tamanho_palavra == 0:
                 print("Você perdeu")
-                encerrado = True
-            elif verificacao == True and forca.vidas == 0:
-                print("Vocês perderam! Ninguém ganhou! Todo mundo empatou!")
-                encerrado = True
+                exit(1)
             else:
                 print("Sua vez de jogar!")
+        elif codigo == 'H':
+            cls()
+            print('Seu oponente acertou a palavra!')
+            print('A palavra era: ' + forca.palavra)
+            exit(1)
         else:
             print('Erro')
             sys.exit(-2)
@@ -132,21 +140,57 @@ def main():
             # Servidor começa jogando (Tentar implementar início random depois)
             forca.escreve_palavra()
             print('Vidas restantes: ' + str(forca.vidas))
-            print('As letras já usadas foram' + forca.lista_letras_usadas)
-            letra = forca.tenta_letra()
+            print('As letras já usadas foram:')
+            if len(forca.lista_letras_usadas) > 0:
+                output = ''
+                for i in range(len(forca.lista_letras_usadas)):
+                    output += forca.lista_letras_usadas[i] + ' '
+                print(output)
 
-            while letra.isnumeric():
-                print('Por favor utilize apenas letras unicas!')
+            selecao = input('Você deseja chutar a palavra?\nDigite 1 para sim\nDigite 2 para não\n')
+            while not selecao.isnumeric() or int(selecao) < 1 or int(selecao) > 2:
+                selecao = input('Por favor, selecione apenas entre 1 e 2!\n')
+
+            if int(selecao) == 2:
                 letra = forca.tenta_letra()
 
-            forca.verifica_letra(letra)
+                while letra.isnumeric():
+                    print('Por favor utilize apenas letras unicas!')
+                    letra = forca.tenta_letra()
 
-            msg = 'L'.encode()
-            msg += letra.encode()
+                code = forca.verifica_letra(letra)
+                
+                # Se a letra ja foi usada, pede por uma nova
+                if code == -1:
+                    letra = forca.tenta_letra()
+                    while letra.isnumeric() or (code == -1):
+                        print('Por favor utilize apenas letras unicas!')
+                        letra = forca.tenta_letra()
+                        code = forca.verifica_letra(letra)
+                    
+                msg = 'L'.encode()
+                msg+= letra.encode()
+                jogador.send(msg)
 
-            jogador.send(msg)
+                # Envia a letra ao oponente antes de encerrar o jogo
+                encerrado = forca.verifica_fim_do_jogo()
+            else:
+                chute = input('Digite seu chute:').upper()
+                
+                if chute == forca.palavra:
+                    cls()
+                    print('Parabens!\nA palavra era: ' + forca.palavra)
+                    jogador.send('H'.encode())
+                    print('Você ganhou!')
+                    exit(1)
+                else:
+                    print('Você errou!')
+                    jogador.send('M'.encode())
+
             # Envia a letra ao oponente antes de encerrar o jogo
             encerrado = forca.verifica_fim_do_jogo()
+        else:
+            print('Voce ganhou!')
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -23,7 +23,8 @@ class main_game:
 
         if letra in self.lista_letras_usadas:
             print('Essa letra ja foi utilizada!')
-        #Resolver problema de letras repetidas
+            return -1
+
         elif letra in self.lista_letras:
             print('Letra encontrada! \"' + letra + '\"')
             self.lista_letras_usadas.append(letra)
@@ -58,7 +59,7 @@ class main_game:
         
         elif self.vidas == 0:
             cls()
-            print('Que pena, voce perdeu :<\nA palavra era: ' + self.palavra)
+            print('Que pena, voces perderam :<\nA palavra era: ' + self.palavra)
             self.fim_do_jogo = True
         
         else:
@@ -67,21 +68,22 @@ class main_game:
         return self.fim_do_jogo
 
     def escreve_palavra(self):
-
+        output = ''
         for i in range(self.tamanho_original):
             # se letra ja encontrada, printa a letra e um espaco
             if self.lista_letras[i] in self.lista_letras_usadas:
-                print(self.lista_letras[i] + ' ')
+                output+=self.lista_letras[i] + ' '
             else:
-                print('_ ')
+                output+='_ '
+        print(output)
 
     def tenta_letra(self):
         letra = input('Digite uma letra que deseja testar:')
-        return letra
+        return letra.capitalize()
 
 
 def main():
-
+    cls()
     socketConexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     endereco = ('127.0.0.1', 50000)
     socketConexao.bind(endereco)
@@ -90,59 +92,96 @@ def main():
     [jogador, _] = socketConexao.accept()
 
     forca = main_game("BATATA")
-    #Servidor tem que enviar palavra para o cliente para que o jogo inicie
+    # Servidor tem que enviar palavra para o cliente para que o jogo inicie
     msg = 'P'.encode()
     msg += forca.tamanho_original.to_bytes(1, byteorder='big')
     msg += forca.palavra.encode()
     jogador.send(msg)
 
-    encerrado = False
+    encerrado = forca.fim_do_jogo
 
     while not encerrado:
-
-        #Servidor começa jogando (Tentar implementar início random depois)
+        # Servidor começa jogando (Tentar implementar início random depois)
         forca.escreve_palavra()
         print('Vidas restantes: ' + str(forca.vidas))
-        print('As letras já usadas foram' + forca.lista_letras_usadas)
-        letra = forca.tenta_letra()
+        print('As letras já usadas foram:')
+        if len(forca.lista_letras_usadas) > 0:
+            output = ''
+            for i in range(len(forca.lista_letras_usadas)):
+                output += forca.lista_letras_usadas[i] + ' '
+            print(output)
 
-        while letra.isnumeric():
-            print('Por favor utilize apenas letras unicas!')
+        selecao = input('Você deseja chutar a palavra?\nDigite 1 para sim\nDigite 2 para não\n')
+        while not selecao.isnumeric() or int(selecao) < 1 or int(selecao) > 2:
+            selecao = input('Por favor, selecione apenas entre 1 e 2!\n')
+
+        if int(selecao) == 2:
             letra = forca.tenta_letra()
 
-        forca.verifica_letra(letra)
+            while letra.isnumeric():
+                print('Por favor utilize apenas letras unicas!')
+                letra = forca.tenta_letra()
 
-        msg = 'L'.encode()
-        msg+= letra.encode()
+            code = forca.verifica_letra(letra)
+            
+            # Se a letra ja foi usada, pede por uma nova
+            if code == -1:
+                letra = forca.tenta_letra()
+                while letra.isnumeric() or (code == -1):
+                    print('Por favor utilize apenas letras unicas!')
+                    letra = forca.tenta_letra()
+                    code = forca.verifica_letra(letra)
+                
+            msg = 'L'.encode()
+            msg+= letra.encode()
+            jogador.send(msg)
 
-        jogador.send(msg)
-        #Envia a letra ao oponente antes de encerrar o jogo
-        encerrado = forca.verifica_fim_do_jogo()
+            # Envia a letra ao oponente antes de encerrar o jogo
+            encerrado = forca.verifica_fim_do_jogo()
+        else:
+            chute = input('Digite seu chute:').upper()
+            
+            if chute == forca.palavra:
+                cls()
+                print('Parabens!\nA palavra era: ' + forca.palavra)
+                jogador.send('H'.encode())
+                print('Você ganhou!')
+                exit(1)
+            else:
+                print('Você errou!')
+                jogador.send('M'.encode())
 
-        #Retorno já é a jogada do oponente
-
+        # Retorno já é a jogada do oponente
         if not encerrado:
-
+            cls()
             codigo = jogador.recv(1)
+
             if not codigo:
                 sys.exit(-1)
+
             codigo = codigo.decode()
+
             if codigo == 'L':
                 letra = jogador.recv(1)
                 letra = letra.decode()
                 forca.verifica_letra_oponente(letra)
-                verificacao = forca.verifica_fim_do_jogo() #Atualizar verificação para oponente
+                verificacao = forca.verifica_fim_do_jogo() # Atualizar verificação para oponente
+
                 if verificacao == True and forca.tamanho_palavra == 0:
                     print("Você perdeu")
-                    encerrado = True
-                elif verificacao == True and forca.vidas == 0:
-                    print("Vocês perderam! Ninguém ganhou! Todo mundo empatou!")
-                    encerrado = True
+                    exit(1)
                 else:
                     print("Sua vez de jogar!")
+            elif codigo == 'H':
+                cls()
+                print('Seu oponente acertou a palavra!')
+                print('A palavra era: ' + forca.palavra)
+                exit(1)
             else:
                 print('Erro')
                 sys.exit(-2)
+        else:
+            print('Voce ganhou!')
 
 if __name__ == '__main__':
     main()
